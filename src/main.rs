115345f78c2aa48586863e20603989130abbaca3
Joris van de Donk <jorisvddonk@gmail.com>
2022-07-23 17:24:35 +0200

    draw starmap in 3d using Raylib

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use std::{
 };
 use structopt::StructOpt; // 1.2.7
 use regex::Regex;
+use raylib::prelude::*;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -27,6 +28,12 @@ struct Star {
     _unused: i32,
     name: AsciiString,
     typestr: AsciiString,
+}
+
+impl std::convert::From<&Star> for raylib::ffi::Vector3 {
+    fn from(star: &Star) -> Self {
+        return raylib::ffi::Vector3 {x: star.x  as f32 * 0.0000001, y: star.y as f32 * 0.0000001, z: star.z as f32 * 0.0000001};
+    }
 }
 
 lazy_static! {
@@ -87,9 +94,9 @@ impl Star {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut stars: Vec<Star> = Vec::new();
     let args = Cli::from_args();
     let file = File::open(&args.path).unwrap();
-
     let mut i = 0;
     loop {
         let star = Star::from_reader(&file);
@@ -100,15 +107,53 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     "Star/Planet: [{}] {} {} {} {} {} {}",
                     i, star.x, star.y, star.z, star.name, star.index, star.typestr
                 );
+                stars.push(star);
             }
             Err(error) => {
                 // malformed entry OR EOF
                 // exit if EOF, ignore otherwise
                 if error.kind() == ErrorKind::UnexpectedEof {
-                    return Ok(());
+                    break;
                 }
                 eprintln!("Malformed entry at [{}]; {}", i, error.to_string());
             }
         };
     }
+
+    let (mut rl, thread) = raylib::init()
+    .size(640, 480)
+    .title("Starmap2 viewer")
+    .build();
+
+    let mut camera = Camera3D::perspective(
+        Vector3::new(4.0, 4.0, 4.0),  // Position
+        Vector3::new(0.0, 0.0, 0.0), // Target
+        Vector3::new(0.0, 1.0, 0.0),  // Up vector
+        45.0,                         // FOV
+    );
+
+    let up_vec = raylib::ffi::Vector3 { x: 0.0, y: 1.0, z: 0.0};
+
+    let disp_str = format!("Displaying coordinates of {} named objects", stars.len());
+ 
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+        
+        d.clear_background(Color::WHITE);
+        d.draw_text(disp_str.as_str(), 12, 32, 20, Color::BLACK);
+        d.draw_fps(12, 12);
+
+        {
+            let mut mode_3d = d.begin_mode3D(camera);
+
+            mode_3d.draw_grid(10, 1.0);
+            for s in &stars {
+                //mode_3d.draw_point3D(s, Color::BLACK);
+                mode_3d.draw_circle_3D(s, 0.01, up_vec, 0.0, Color::BLACK)
+            }
+        }
+ 
+    }
+
+    return Ok(());
 }
